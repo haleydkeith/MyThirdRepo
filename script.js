20e4cd68603c337f085b2d4f4c5dda38561a60fa
haleydkeith <haleydkeith0905@gmail.com>
2020-05-11 13:22:31 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const TicTacToeGame = new TicTacToeGame ();
+TicTacToeGame.start()
+
+Function TicTacToeGame () {
+const board = new board();
+const humanPlayer = new humanPlayer();
+const computerPlayer = new computerPlayer();
+let turn = 0;
+
+
+
+this.start = Function() {
+    const config = { childList: true} 
+
+}
+}
+
+Function board() {
+    this.positions = Array.from(document.querySelectorAll(HTMLTableRowElement)
+Console.console.log(this.positions) ;
+
+
+}
+Function computerPlayer() {
+
+}
+Function humanPlayer() {
+
+}
